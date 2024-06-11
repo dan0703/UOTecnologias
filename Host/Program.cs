319e523fec1d076c13d5c64b54dfbfdb84226c73
dan0703 <dansegura8863@gmail.com>
2024-06-11 17:33:11 -0600

    Creación de la estructura del proyecto, Host configurado

---
+++ Host/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Service;
+
+
+namespace Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Implementations)))
+            {
+                host.Open();
+
+                Console.WriteLine("Host is starting");
+                Console.ReadLine();
+            }
+        }
+    }
+}
