7b766d06ad09c48caf5b1a521716a056dfef5998
dan0703 <dansegura8863@gmail.com>
2024-06-14 00:16:52 -0600

    Sugerencias del analisis estatico de codigo atendidas

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ServiceModel;
 using Service;
-using DataAccess;
-
 
 namespace Host
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
